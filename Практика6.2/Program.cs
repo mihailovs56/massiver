9f2731bee544f1ae4c44cc50c4738bb8c9514e55
Михайлов <@DESKTOP-84VD5MI>
2023-10-21 20:49:24 +0500

    Добавьте файлы проекта.

---
+++ Практика6.2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практика6._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину массива: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int[] mas = new int[x];
+            int b = -1;
+            int c = 1;
+            
+            for(int i = x; i > 0; i--)
+            {
+                b += i * c;
+                c = -c;
+                Console.WriteLine(b.ToString());
+                mas[b] = b;
+            }
+            
+            Console.WriteLine(string.Join(",", mas));
+        }
+    }
+}
